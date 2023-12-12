ec65fbe2e82530aec1c7dd3d816e9e8e68ea1d02
Natalie Benjes <natalie.benjes@gmail.com>
2023-12-12 14:15:47 -0800

    add file extensions to html and vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  extensions: ['.js', '.jsx'],
   base: '/mastermind2/',
   plugins: [react()],
 });
+
