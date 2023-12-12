bccc8368fd94aaadae4ad17dd3469b4211d523e2
Natalie Benjes <natalie.benjes@gmail.com>
2023-12-12 14:57:09 -0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   extensions: ['.js', '.jsx'],
+  base: '/mastermind2/',
   plugins: [react()],
 });
 
