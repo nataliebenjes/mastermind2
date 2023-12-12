d2ff071f88181b54265c3a47edf32f92ed85b962
Natalie Benjes <natalie.benjes@gmail.com>
2023-12-12 14:37:02 -0800

    omit base from vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   extensions: ['.js', '.jsx'],
-  base: '/matermind2/',
   plugins: [react()],
 });
 
